b803f793cb3df5237a00acb55df54b7062d56536
Vitalya1991 <convinceall@mail.ru>
2023-05-06 01:07:53 +0300

    Storage item and user storage refactor

--- src/main/java/ru/practicum/shareit/item/storage/ItemStorageImpl.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemStorageImpl.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ItemStorageImpl implements ItemStorage{
+public class ItemStorageImpl implements ItemStorage {
     private final Map<Long, Item> itemMap = new HashMap<>();
 
     private final UserService userService;
